9bc986e066ae766552f6e084fecab2c40d7e2dc3
Krishna Gupta <krishnagupta63000@gmail.com>
2026-08-21 13:15:11 +0530

    added classroom

---
+++ Classroom.py
@@ -0,0 +1,8 @@
+#Q.
+'''You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students.
+"python",
+" "java", "C++", "python", "javascript",
+"java", "python", "java", "C++", "C"'''
+
+set = {'jawa', 'python', "C++", "python", "jawascript", "jawa", "python", "jawa", "C++", "C"}
+print(len(set), "Classrooms")
