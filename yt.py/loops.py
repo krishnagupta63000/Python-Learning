f2c15405e3ad833bdd2f2dc78ac1ef89dfb4fbd9
Krishna Gupta <krishnagupta63000@gmail.com>
2026-08-23 16:04:52 +0530

    Added loops program

---
+++ yt.py/loops.py
@@ -0,0 +1,45 @@
+'''Print numbers from 1 to 100.'''
+
+'''i = 1
+while (i<=100):
+    print(i)
+    i += 1'''
+
+'''Print numbers from 100 to 1.'''
+
+'''i = 100
+while (i>=1):
+    print(i)
+    i -= 1'''
+
+'''Print the multiplication table of a number n.'''
+
+'''n = int(input("Enter Number: "))
+i = 1
+while(i<=10):
+    print(n,"x",i,"=",n*i)
+    i+=1'''
+
+'''Print the elements of the following list using a loop:
+[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]'''
+
+'''nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+i = 0
+while (i<=(len(nums)-1)):
+    print(nums[i])
+    i+=1'''
+
+'''Search for a number x in this tuple using loop:
+(1, 4, 9, 16, 25, 36, 49, 64, 81, 100)'''
+
+'''nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+x = int(input("Enter x : "))
+i = 0
+while (i < len(nums)):
+    if (x == nums[i]):
+        print("Found at index : ", i)
+        break
+    i+=1
+else:
+    print("Not Present in the Tuple.s")'''
+
