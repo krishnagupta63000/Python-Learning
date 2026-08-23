008f13a39a596af484e7643dd4afc65f99eb3403
Krishna Gupta <krishnagupta63000@gmail.com>
2026-08-23 16:17:47 +0530

    added new Question

--- yt.py/loops.py
+++ yt.py/loops.py
@@ -43,3 +43,23 @@ while (i < len(nums)):
 else:
     print("Not Present in the Tuple.s")'''
 
+'''Print the elements of the following list using a loop:
+[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]'''
+
+'''list = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+for i in list:
+    print(i)'''
+
+'''Search for a number x in this tuple using loop:
+[1, 4, 9, 16, 25, 36, 49, 64, 81,100]'''
+
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+x = int(input("Enter x : "))
+i = 0
+for val in nums:
+    if (x == val):
+        print("Found at index : ", i)
+        break
+    i += 1
+else:
+    print("Not Found.")
