cbfbcc0a257523bc39f7e382938db142a262a711
Krishna Gupta <krishnagupta63000@gmail.com>
2026-08-23 16:36:19 +0530

    updated loops.py

--- yt.py/loops.py
+++ yt.py/loops.py
@@ -71,5 +71,11 @@ else:
 
 '''Print numbers from 100 to 1.'''
 
-for i in range(100,0,-1):
-    print(i)
+'''for i in range(100,0,-1):
+    print(i)'''
+
+'''Print the multiplication table of a number n.'''
+
+n = int(input("Enter N: "))
+for i in range(1,11):
+    print(n*i)
