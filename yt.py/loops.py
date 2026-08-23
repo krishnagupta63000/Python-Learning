970225ce3a9b93edd4d476309e3b06dd2b35a1cc
Krishna Gupta <krishnagupta63000@gmail.com>
2026-08-23 16:32:52 +0530

    added new question

--- yt.py/loops.py
+++ yt.py/loops.py
@@ -66,5 +66,10 @@ else:
 
 '''Print numbers from 1 to 100.'''
 
-for i in range(1,101):
+'''for i in range(1,101):
+    print(i)'''
+
+'''Print numbers from 100 to 1.'''
+
+for i in range(100,0,-1):
     print(i)
