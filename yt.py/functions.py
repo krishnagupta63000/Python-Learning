af49cd918917adeb540e70a980ae10dec7b124e6
Krishna Gupta <krishnagupta63000@gmail.com>
2026-08-25 13:30:25 +0530

    added functions.py

---
+++ yt.py/functions.py
@@ -0,0 +1,10 @@
+# WAF to print the length of the list. [List is the parameter]
+
+cities = ["Jaipur", "Delhi", "Mumbai", "Chennai", "Noida"]
+heroes = ["Captain America", "Spideran", "Batman", "Superman"]
+
+def lenOfcities(list):
+    print(len(list))
+
+lenOfcities(cities)
+lenOfcities(heroes)
