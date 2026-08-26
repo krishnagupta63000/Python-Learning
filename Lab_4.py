24ecbb04b836eabb824fe091928e8b91cc5745b2
Krishna Gupta <krishnagupta63000@gmail.com>
2026-08-26 22:26:43 +0530

    added new question

--- Lab_4.py
+++ Lab_4.py
@@ -115,3 +115,49 @@ if (playing_time >= total):
     print("can sleep well",(playing_time - total,"Hours"))
 else:
     print("cant sleep well",total - playing_time,"Hours")
+
+'''Most of the people start planning their vacations well in advance. A young programmer from
+Bulgaria has a certain budget (BGN is the currency of Bulgaria) and spare time in a particular
+season.
+Write a program that accepts as input the budget and season and as output displays
+programmer's vacation place and the amount of money they will spend.
+The budget determines the destination, and the season determines what amount of the budget
+will be spent. If the season is summer, the programmer will go camping, if it is winter – he will stay
+in a hotel. If it is in Europe, regardless of the season, the programmer will stay in a hotel. Each
+camp or hotel, according to the destination, has its price, which corresponds to a particular
+percentage of the budget:
+• If 100 BGN or less – somewhere in Bulgaria.
+Summer – 30% of the budget
+Winter – 70% of the budget.
+• If 1000 BGN or less – somewhere in the Balkans.
+Summer – 40% of the budget.
+Winter – 80% of the budget.
+• If more than 1000 BGN – somewhere in Europe.
+Upon traveling in Europe, regardless of the season, the programmer will
+spend 90% of the budget.'''
+
+budget = int(input("Enter Budget : "))
+s = input("Enter Season : ")
+
+if (budget<=100):
+    print("Bulgaria")
+    if (s == 'summer'):
+        print("Camping")
+        print("Budget : ", budget*0.3)
+    else:
+        print("Hotel")
+        print("Budget : ", budget*0.7)
+elif (budget<=1000):
+    print("Balkans")
+    if (s == 'summer'):
+        print("Camping")
+        print("Budget : ", budget*0.4)
+    else:
+        print("Hotel")
+        print("Budget : ", budget*0.8)
+else:
+    print("Europe")
+    print("Hotel")
+    print("Budget : ", budget*0.9)
+
+
