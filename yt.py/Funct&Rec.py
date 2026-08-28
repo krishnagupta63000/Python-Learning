741b125ce5a1f7fd848a2fe8ddf504507cfbb987
Krishna Gupta <krishnagupta63000@gmail.com>
2026-08-28 20:00:15 +0530

    added mini function

--- yt.py/Funct&Rec.py
+++ yt.py/Funct&Rec.py
@@ -1,6 +1,11 @@
 #average of 3 numbers
-def avgOf (a, b, c):
+'''def avgOf (a, b, c):
     print(avgOf)
     return (a+b+c)/3
 
-print(avgOf(1, 2, 3))
+print(avgOf(1, 2, 3))'''
+
+# product of 3 numbers 
+def product(a, b, c):
+    return a*b*c
+print(product(2,3,4))
