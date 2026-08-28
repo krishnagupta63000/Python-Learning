73ece6067ebf3e25246a2ec73cc00951691827da
Krishna Gupta <krishnagupta63000@gmail.com>
2026-08-28 20:19:03 +0530

    added new program

--- yt.py/Funct&Rec.py
+++ yt.py/Funct&Rec.py
@@ -6,6 +6,15 @@
 print(avgOf(1, 2, 3))'''
 
 # product of 3 numbers 
-def product(a, b, c):
+'''def product(a, b, c):
     return a*b*c
-print(product(2,3,4))
+print(product(2,3,4))'''
+
+# WAF to print the length of a list. (list is the parameter)
+
+fruits = ['apple', 'banana', 'orange']
+marks = [90, 98, 75, 74, 78]
+def lengthOf(list):
+    print(len(list))
+lengthOf(fruits)
+lengthOf(marks)
