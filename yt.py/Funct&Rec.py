d4b7f208c6662392d1fd81b039f712118a81f205
Krishna Gupta <krishnagupta63000@gmail.com>
2026-08-28 20:26:11 +0530

    added new program

--- yt.py/Funct&Rec.py
+++ yt.py/Funct&Rec.py
@@ -12,9 +12,16 @@ print(product(2,3,4))'''
 
 # WAF to print the length of a list. (list is the parameter)
 
-fruits = ['apple', 'banana', 'orange']
+'''fruits = ['apple', 'banana', 'orange']
 marks = [90, 98, 75, 74, 78]
 def lengthOf(list):
     print(len(list))
 lengthOf(fruits)
-lengthOf(marks)
+lengthOf(marks)'''
+
+marks = [90, 98, 75, 74, 78]
+def elementsof(list):
+    for i in range (5):
+        print(list[i])
+
+elementsof(marks)
