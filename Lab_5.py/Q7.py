ad642e7330bb934fa5440618d913c40896198314
Krishna Gupta <krishnagupta63000@gmail.com>
2026-09-03 22:07:54 +0530

    last 2 problems are very hard

---
+++ Lab_5.py/Q7.py
@@ -0,0 +1,5 @@
+# Take an integer as input and check if it is prime or not. Handle invalid conditions and make your
+# code efficient by minimizing the number of loop iterations.
+
+n = int(input("Enter Number : "))
+if ()
