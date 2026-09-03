b7be377008ce01252e5df02f6a606f7c3e65fc71
Krishna Gupta <krishnagupta63000@gmail.com>
2026-09-03 22:11:27 +0530

    added question 8

---
+++ Lab_5.py/Q8.py
@@ -0,0 +1,2 @@
+# Take a sentence as input and using while loop count the number of capital letters, small letters,
+# digits, and special characters in the sentence. Do not use any inbuilt function.
