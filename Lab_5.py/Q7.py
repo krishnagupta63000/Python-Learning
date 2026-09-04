d6deeda827fac103afac38db79425347adeba32c
Krishna Gupta <krishnagupta63000@gmail.com>
2026-09-04 23:11:27 +0530

    Understood and solved Q7

--- Lab_5.py/Q7.py
+++ Lab_5.py/Q7.py
@@ -2,4 +2,21 @@
 # code efficient by minimizing the number of loop iterations.
 
 n = int(input("Enter Number : "))
-
+if (n<=1):
+    print("Not a Prime Number")
+elif(n == 2):
+    print("Prime Number ")
+elif(n%2==0):
+    print("Not a Prime Number")
+else: 
+    prime = True
+    i = 3
+    while(i**2 <= n):
+        if (n % i == 0):
+            prime = False
+            break 
+        i = i + 2
+    if (prime):
+        print("Its a Prime Number")
+    else:
+        print("Its not a Prime Number")
