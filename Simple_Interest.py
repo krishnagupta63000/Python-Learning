6df5cbc38daca5d8e642eaedcad983b86b5b90bf
Krishna Gupta <krishnagupta63000@gmail.com>
2026-09-05 22:52:45 +0530

    GFG new question

---
+++ Simple_Interest.py
@@ -0,0 +1,5 @@
+p = int(input("Enter Principle Amount : "))
+r = int(input("Enter Rate of Interest : "))
+t = int(input("Enter amount of Time : "))
+si = (p*r*t)/100
+print("Simple Interest : ", si)
