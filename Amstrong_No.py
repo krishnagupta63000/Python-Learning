4b82a8b6bd5c8ceb1a7ff62a68acc681bfe25957
Krishna Gupta <krishnagupta63000@gmail.com>
2026-09-07 21:08:51 +0530

    updated amstrong no

--- Amstrong_No.py
+++ Amstrong_No.py
@@ -1,13 +1,10 @@
-n = int(input("Enter a number: "))
-
+n =   int(input("Enter a number: "))
 original = n
 sum = 0
-
 while n > 0:
     digit = n % 10
     sum = sum + digit ** 3
     n = n // 10
-
 if sum == original:
     print("Armstrong Number")
 else:
