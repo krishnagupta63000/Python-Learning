d06fda92d9301ca2a100634f7193661740472f21
Krishna Gupta <krishnagupta63000@gmail.com>
2026-09-22 22:55:06 +0530

    Added new question

--- MoreOnLoops.py
+++ MoreOnLoops.py
@@ -7,3 +7,14 @@ i = 1
 while i <= n:
     print(i)
     i += 1
+
+
+
+# WAP to print all natural numbers in reverse (from n to 1). - using while loop
+
+
+n = int(input("Enter N: "))
+
+while n >= 1:
+    print(n)
+    n -= 1
