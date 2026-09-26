fc3c6c30189b936968ca980bef797e8c0e5e71c7
Krishna Gupta <krishnagupta63000@gmail.com>
2026-09-26 12:40:21 +0530

    solved q2

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 # num = [1, 2, 4, 5]
 # print(sum(num))
 
-for i in range (3):
-    for j in range (3):
-        if i == j:
-            break
-        else:
-            print("Inner Completed for i =", i)
-    print("Outer continues, i =", i)
+# for i in range (3):
+#     for j in range (3):
+#         if i == j:
+#             break
+#         else:
+#             print("Inner Completed for i =", i)
+#     print("Outer continues, i =", i)
+
+
+
+a = 256
+b = 77
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
 
