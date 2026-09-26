7c6f435b0da2e9f62de6816f202c41f9b4abbaa8
Krishna Gupta <krishnagupta63000@gmail.com>
2026-09-26 12:37:33 +0530

    solved q1 of paper

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-num = [1, 2, 4, 5]
-print(sum(num))
+# num = [1, 2, 4, 5]
+# print(sum(num))
+
+for i in range (3):
+    for j in range (3):
+        if i == j:
+            break
+        else:
+            print("Inner Completed for i =", i)
+    print("Outer continues, i =", i)
+
